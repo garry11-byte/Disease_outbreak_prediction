05e19522800d86757fdaa6067f15fc15d1bf007f
DevdattaShelar <devdatta_shelar_entc@moderncoe.edu.in>
2025-02-13 22:55:55 +0530

    commit

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import find_packages,setup
 from typing import List
-def get_requirements(file_path:str)->list[str]:
-    '''
-    this function will return a list of requirements
-    '''
-    requirements=[]
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path: str) -> List[str]:  # Use List[str]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
 setup(
     name='Disease Outbreak Project',
     version='1.0.0',
